def862fa39752e1f167225416e421896a93c634b
hamza <hamza.alaydi.99@gmail.com>
2025-02-28 23:26:35 +0200

    change bk linke to subdomain

--- src/Root.route.js
+++ src/Root.route.js
@@ -1,3 +1,5 @@
 // export const rootRoute = "http://localhost:5000/api";
-export const rootRoute = "http://15.157.230.241/api";
+export const rootRoute = "https://mms.stephanosbakedgoods.com/api";
+// export const rootRoute = "http://15.157.230.241/api";
+
 // export const rootRoute = "";
