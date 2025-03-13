25c65d7529a68dedc200ae4ec2f98eb59f4e71f6
hamza <hamza.alaydi.99@gmail.com>
2025-03-13 21:42:38 +0200

    fix

--- src/Root.route.js
+++ src/Root.route.js
@@ -1,5 +1,5 @@
-export const rootRoute = "http://localhost:5000/api";
-// export const rootRoute = "https://mms.stephanosbakedgoods.com/api";
+// export const rootRoute = "http://localhost:5000/api";
+export const rootRoute = "https://mms.stephanosbakedgoods.com/api";
 export const S3 =
   "https://stephano-group-attachments-bucket-ca.s3.ca-central-1.amazonaws.com";
 // export const rootRoute = "";
