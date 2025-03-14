6277f1813cdbbc573f208eceb393f1a637415b37
hamza <hamza.alaydi.99@gmail.com>
2025-03-14 07:56:18 +0200

    check if back with front

--- src/Root.route.js
+++ src/Root.route.js
@@ -3,3 +3,5 @@ export const rootRoute = "https://mms.stephanosbakedgoods.com/api";
 export const S3 =
   "https://stephano-group-attachments-bucket-ca.s3.ca-central-1.amazonaws.com";
 // export const rootRoute = "";
+
+
